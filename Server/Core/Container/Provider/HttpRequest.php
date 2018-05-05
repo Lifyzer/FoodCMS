533678c6a11beb102602b7ad9fc5220c396e70df
Pierre-Henry Soria <hi@ph7.me>
2018-05-05 16:30:36 +0100

    Add HttpRequest container

---
+++ Server/Core/Container/Provider/HttpRequest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@lifyzer.com>
+ * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types=1);
+
+namespace Lifyzer\Server\Core\Container\Provider;
+
+use Symfony\Component\HttpFoundation\Request;
+
+class HttpRequest implements Providable
+{
+    public function getService(): Request
+    {
+        return Request::createFromGlobals();
+    }
+}
---
+++ Test/Server/Core/Container/Provider/HttpRequestTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@lifyzer.com>
+ * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types=1);
+
+namespace Lifyzer\Test\Server\Core\Container\Provider;
+
+use Lifyzer\Server\Core\Container\Provider\HttpRequest;
+use Lifyzer\Server\Core\Container\Provider\Providable;
+use Phake;
+use Phake_IMock;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\Request;
+
+class HttpRequestTest extends TestCase
+{
+    public function testGetService(): void
+    {
+        /** @var Providable|Phake_IMock $httpRequest */
+        $httpRequest = Phake::mock(HttpRequest::class);
+        $this->assertInstanceOf(Request::class, $httpRequest->getService());
+    }
+}
