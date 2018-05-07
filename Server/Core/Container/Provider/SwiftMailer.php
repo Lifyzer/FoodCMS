37c03a1037880d2fc3586dae4e3b06e0f69e86ec
Pierre-Henry Soria <hi@ph7.me>
2018-05-07 17:31:01 +0100

    Add SwiftMailer Container

---
+++ Server/Core/Container/Provider/SwiftMailer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@lifyzer.com>
+ * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types=1);
+
+namespace Lifyzer\Server\Core\Container\Provider;
+
+use Swift_Mailer;
+use Swift_SendmailTransport;
+
+class SwiftMailer implements Providable
+{
+    public function getService(): Swift_Mailer
+    {
+        $transport = new Swift_SendmailTransport();
+
+        return new Swift_Mailer($transport);
+    }
+}
---
+++ Test/Server/Core/Container/Provider/SwiftMailer.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@lifyzer.com>
+ * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ */
+
+declare(strict_types=1);
+
+namespace Lifyzer\Test\Server\Core\Container\Provider;
+
+use Lifyzer\Server\Core\Container\Provider\Providable;
+use Lifyzer\Server\Core\Container\Provider\SwiftMailer;
+use Phake;
+use Phake_IMock;
+use PHPUnit\Framework\TestCase;
+use Swift_Mailer;
+
+class SwiftMailerTest extends TestCase
+{
+    public function testGetService(): void
+    {
+        /** @var Providable|Phake_IMock $swiftMailer */
+        $swiftMailer = Phake::mock(SwiftMailer::class);
+        $this->assertInstanceOf(Swift_Mailer::class, $swiftMailer->getService());
+    }
+}
