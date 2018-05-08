f5b039af1794ecd5f729a5182c1b0c8aac3d3395
Pierre-Henry Soria <hi@ph7.me>
2018-05-08 19:26:46 +0100

    Backup easily the project on different git hosting services

---
+++ save-project.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+function save-project-to-repo() {
+    git remote rm origin
+    git remote add origin $1
+    git push
+}
+
+gitRemotes=(
+    git@github.com:Lifyzer/Lifyzer-WebApp-CMS.git
+    git@bitbucket.org:pH_7/lifyzer-webapp-cms.git
+    git@gitlab.com:pH-7/lifyzer-webapp-cms.git
+)
+for remote in "${gitRemotes[@]}"
+do
+    save-project-to-repo $remote
+done
