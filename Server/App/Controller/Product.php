ab8cca0b584c28f49860f05a2d89a6234a18e478
Pierre-Henry Soria <hi@ph7.me>
2019-01-14 07:49:24 +1300

    Update Product controller for the needs of FoodCMS :)

--- Server/App/Controller/Product.php
+++ Server/App/Controller/Product.php
@@ -22,11 +22,8 @@ use Symfony\Component\HttpFoundation\ParameterBag;
 
 class Product extends Base
 {
-    private const ADD_PRODUCT_VIEW_FILE = 'product/add.twig';
-    private const SUBMIT_PRODUCT_VIEW_FILE = 'product/submit.twig';
-    private const EMAIL_NEW_PRODUCT_VIEW_FILE = 'emails/new-product-details.twig';
-    private const EMAIL_SUBJECT = 'New Product to be moderated';
-    private const HTML_CONTENT_TYPE = 'text/html';
+    private const INDEX_PRODUCT_VIEW_FILE = 'product/homepage.twig';
+    private const SHOW_PRODUCT_VIEW_FILE = 'product/show.twig';
 
     /** @var ProductModel */
     private $productModel;
@@ -46,10 +43,10 @@ class Product extends Base
         $this->container = $container;
     }
 
-    public function add(): void
+    public function homepage(): void
     {
         $this->view->display(
-            self::ADD_PRODUCT_VIEW_FILE,
+            self::INDEX_PRODUCT_VIEW_FILE,
             [
                 'siteUrl' => SITE_URL,
                 'siteName' => SITE_NAME,
@@ -58,155 +55,42 @@ class Product extends Base
         );
     }
 
-    public function submit(): void
+    public function show(int $productId): void
     {
-        $request = $this->httpRequest->request;
+        $productData = $this->productModel->get($productId);
 
-        if ($request->get('addproduct') && !$this->isSpamBot($request)) {
-            $data = $request->all();
-
-            // Remove unused params since we don't bind them
-            unset($data['addproduct'], $data['firstname']);
-
-            if (empty($data['barcode'])) {
-                $data['barcode'] = '-';
-            }
-
-            if ($this->isFormCompleted($data)) {
-                try {
-                    if (
-                        !$this->productModel->doesBarcodeExist($data['barcode']) &&
-                        !$this->productModel->doesProductNameExist($data['name'])
-                    ) {
-                        $data['productId'] = $this->productModel->addToPending($data);
-                        $this->sendEmail($data);
-                        $message = 'Product successfully submitted. It will be reviewed shortly';
-                    } else {
-                        $message = 'Oops! This product has already been added to the database.';
-                    }
-
-                    $this->view->display(
-                        self::SUBMIT_PRODUCT_VIEW_FILE,
-                        [
-                            'siteUrl' => SITE_URL,
-                            'siteName' => SITE_NAME,
-                            'pageName' => 'Add a Product',
-                            'message' => $message
-                        ]
-                    );
-                } catch (PDOException $except) {
-                    /** @var LoggerInterface $log */
-                    $log = $this->container->get(Monolog::class);
-                    $log->error(
-                        'PDO Exception',
-                        [
-                            'message' => $except->getMessage(),
-                            'trace' => $except->getTraceAsString()
-                        ]
-                    );
-
-                    (new Error($this->container))->internalError();
-                    exit;
-                }
-            } else {
-                $this->redirectToHomepage();
-            }
+        if (!empty($productData)) {
+            $this->view->display(
+                self::SHOW_PRODUCT_VIEW_FILE,
+                [
+                    'siteUrl' => SITE_URL,
+                    'siteName' => SITE_NAME,
+                    'pageName' => 'Add a Product',
+                    'item' => $productData
+                ]
+            );
         } else {
             $this->redirectToHomepage();
         }
     }
 
-    public function approve(array $data): void
+    public function show(int $productId): void
     {
-        if ($this->isSecurityHashValid($data)) {
-            $productId = (int)$data['id'];
-            $result = $this->productModel->moveToLive($productId);
-            echo $result ? 'Product approved! :)' : 'An error occurred...';
-        } else {
-            echo 'Invalid security hash!';
-        }
-    }
+        $productData = $this->productModel->get($productId);
 
-    public function disapprove(array $data): void
-    {
-        if ($this->isSecurityHashValid($data)) {
-            $productId = (int)$data['id'];
-            $result = $this->productModel->discard($productId);
-            echo $result ? 'Product discard! :(' : 'An error occurred...';
-        } else {
-            echo 'Invalid security hash!';
-        }
-    }
-
-    private function sendEmail(array $data): void
-    {
-        $adminEmail = getenv('ADMIN_EMAIL');
-
-        $urls = [
-            'approvalUrlHash' => $this->getApprovalUrl($data['productId']),
-            'disapprovalUrlHash' => $this->getDisapprovalUrl($data['productId'])
-        ];
-
-        $message = (new Swift_Message(self::EMAIL_SUBJECT))
-            ->setFrom($adminEmail)
-            ->setTo($adminEmail)
-            ->setBody(
-                $this->view->render(
-                    self::EMAIL_NEW_PRODUCT_VIEW_FILE,
-                    array_merge($data, $urls)
-                ),
-                self::HTML_CONTENT_TYPE
+        if (!empty($productData)) {
+            $this->view->display(
+                self::SHOW_PRODUCT_VIEW_FILE,
+                [
+                    'siteUrl' => SITE_URL,
+                    'siteName' => SITE_NAME,
+                    'pageName' => 'Add a Product',
+                    'item' => $productData
+                ]
             );
-
-        $this->mailer->send($message);
-    }
-
-    private function getApprovalUrl(int $productId): string
-    {
-        return sprintf(
-            '%sapprove/%s/%d',
-            SITE_URL,
-            getenv('SECURITY_HASH'),
-            $productId
-        );
-    }
-
-    private function getDisapprovalUrl(int $productId): string
-    {
-        return sprintf(
-            '%sdisapprove/%s/%d',
-            SITE_URL,
-            getenv('SECURITY_HASH'),
-            $productId
-        );
-    }
-
-    /**
-     * Make sure that a human fulfilled the form (a bot would fulfil "firstname" field as well).
-     *
-     * @param ParameterBag $request
-     *
-     * @return bool
-     */
-    private function isSpamBot(ParameterBag $request): bool
-    {
-        return (bool)$request->get('firstname');
-    }
-
-    private function isFormCompleted(array $fields): bool
-    {
-        foreach ($fields as $name => $value) {
-            if (!isset($name) || trim($value) === '') {
-                return false;
-            }
-
-            return true;
+        } else {
+            $this->redirectToHomepage();
         }
-    }
-
-    private function isSecurityHashValid(array $data): bool
-    {
-        return $data['hash'] === getenv('SECURITY_HASH');
     }
 
     private function redirectToHomepage(): void
