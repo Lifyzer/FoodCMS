e64b412d6b04f116933ecae0e5021067358c9ef1
Pierre-Henry Soria <hi@ph7.me>
2019-01-14 07:49:47 +1300

    Rewrite Product Model; Allow to get product data

--- Server/App/Model/Product.php
+++ Server/App/Model/Product.php
@@ -13,22 +13,11 @@ namespace Lifyzer\Server\App\Model;
 use Lifyzer\Server\Core\Container\Provider\Database;
 use PDO;
 use Psr\Container\ContainerInterface;
+use stdClass;
 
 class Product
 {
-    private const QUERY_PRODUCT_NAME_EXISTS = 'SELECT COUNT(product_name) FROM product WHERE product_name = :product_name LIMIT 1';
-
-    private const QUERY_BARCODE_EXISTS = 'SELECT COUNT(barcode_id) FROM product WHERE barcode_id = :barcode LIMIT 1';
-
-    private const QUERY_ADD_PRODUCT_TO_PENDING = '
-      INSERT INTO pending_product (barcode_id, product_name, ingredients, sugar, carbohydrate, saturated_fats, dietary_fiber, protein, salt, sodium, alcohol, product_image, is_organic, is_healthy)
-      VALUES(:barcode, :name, :ingredients, :sugar, :carbohydrate, :saturatedfat, :fiber, :protein, :salt, :sodium, :alcohol, :image, :isorganic, :ishealthy)';
-
-    private const QUERY_MOVE_PRODUCT_TO_LIVE = '
-      INSERT INTO product (barcode_id, product_name, ingredients, sugar, carbohydrate, saturated_fats, dietary_fiber, protein, salt, sodium, alcohol, product_image, is_organic, is_healthy)
-      SELECT barcode_id, product_name, ingredients, sugar, carbohydrate, saturated_fats, dietary_fiber, protein, salt, sodium, alcohol, product_image, is_organic, is_healthy FROM pending_product WHERE id = :productId LIMIT 1';
-
-    private const QUERY_DELETE_PRODUCT_FROM_PENDING = 'DELETE FROM pending_product  WHERE id = :productId LIMIT 1';
+    private const QUERY_GET_PRODUCT = 'SELECT * FROM product WHERE id = :productId LIMIT 1';
 
     /** @var PDO */
     private $db;
@@ -38,50 +27,12 @@ class Product
         $this->db = $container->get(Database::class);
     }
 
-    public function doesProductNameExist(string $productName): bool
+    public function get(int $productId): stdClass
     {
-        $stmt = $this->db->prepare(self::QUERY_PRODUCT_NAME_EXISTS);
-        $stmt->bindValue('product_name', $productName, PDO::PARAM_STR);
+        $stmt = $this->db->prepare(self::QUERY_GET_PRODUCT);
+        $stmt->bindValue('productId', $productId, PDO::PARAM_STR);
         $stmt->execute();
 
-        return $stmt->fetchColumn() == 1;
-    }
-
-    public function doesBarcodeExist(string $barcode): bool
-    {
-        $stmt = $this->db->prepare(self::QUERY_BARCODE_EXISTS);
-        $stmt->bindValue('barcode', $barcode, PDO::PARAM_STR);
-        $stmt->execute();
-
-        return $stmt->fetchColumn() == 1;
-    }
-
-    /**
-     * @param array $binds
-     *
-     * @return int Returns the product's ID
-     */
-    public function addToPending(array $binds): int
-    {
-        $stmt = $this->db->prepare(self::QUERY_ADD_PRODUCT_TO_PENDING);
-        $stmt->execute($binds);
-
-        return (int)$this->db->lastInsertId();
-    }
-
-    public function moveToLive(int $productId): bool
-    {
-        $stmt = $this->db->prepare(self::QUERY_MOVE_PRODUCT_TO_LIVE);
-        $stmt->bindValue('productId', $productId, PDO::PARAM_INT);
-
-        return $stmt->execute();
-    }
-
-    public function discard(int $productId): bool
-    {
-        $stmt = $this->db->prepare(self::QUERY_DELETE_PRODUCT_FROM_PENDING);
-        $stmt->bindValue('productId', $productId, PDO::PARAM_INT);
-
-        return $stmt->execute();
+        return $stmt->fetchObject();
     }
 }
