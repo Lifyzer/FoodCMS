d6516f7ddc2523237270842bcc795386f1f986d9
Pierre-Henry Soria <hi@ph7.me>
2019-01-14 18:41:00 +1300

    Bumped year!

--- Server/Core/Uri/Router.php
+++ Server/Core/Uri/Router.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author         Pierre-Henry Soria <hello@lifyzer.com>
- * @copyright      (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2018-2019, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
  * @link           https://lifyzer.com
  */
