a6f77a5c923dc50ca0e59d1714f5ae7128688a8d
Pierre-Henry Soria <hi@ph7.me>
2019-01-16 13:22:52 +1300

    Add About page!

---
+++ Server/App/Controller/About.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@lifyzer.com>
+ * @copyright      (c) 2019, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
+ * @link           https://lifyzer.com
+ */
+
+declare(strict_types=1);
+
+namespace Lifyzer\Server\App\Controller;
+
+use Psr\Container\ContainerInterface;
+
+class About extends Base
+{
+    private const ABOUT_IT = 'about/about-it.twig';
+
+    public function __construct(ContainerInterface $container)
+    {
+        parent::__construct($container);
+    }
+
+    public function about(): void
+    {
+        $data = [
+            'siteUrl' => SITE_URL,
+            'siteName' => SITE_NAME,
+            'pageName' => 'About Lifyzer\'s Mission! 😋',
+        ];
+
+        $this->view->display(self::ABOUT_IT, $data);
+    }
+}
