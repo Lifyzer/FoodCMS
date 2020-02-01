40a4b34b0a2c7b05f7f70edd6fd4cc6f5e7cda5f
Pierre-Henry Soria <hi@ph7.me>
2020-02-01 21:27:10 +1030

    [routes.php]Use PHP7 strict type mode

--- Server/config/routes.php
+++ Server/config/routes.php
@@ -1,9 +1,11 @@
 <?php
 /**
  * @author         Pierre-Henry Soria <hello@lifyzer.com>
- * @copyright      (c) 2018-2019, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2018-2020, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; <https://www.gnu.org/licenses/gpl-3.0.en.html>
  */
+
+declare(strict_types=1);
 
 namespace Lifyzer\Server;
 
